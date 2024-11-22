5bc793d19f35cf1dcf8f8596e21c74f641fc7c5a
vaishnavi <vaishnavib@gmail.com>
2024-11-23 00:00:34 +0530

    committing project

---
+++ src/test/java/SEGroup/mavenjavaproject/AppTest.java
@@ -0,0 +1,12 @@
+package SEGroup.mavenjavaproject;
+
+/**
+ * Hello world!
+ */
+public class AppTest
+{
+    public static void main(String[] args)
+    {
+        System.out.println( "This is the first Maven Java Project in Eclipse vaishnavi" );
+    }
+}
